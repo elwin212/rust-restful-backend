f8844f68f7dcf7de21e73c914ec1c41546a9e4df
Elwin <elwin@mmisys.net>
2023-09-14 13:32:33 -0700

    add log fille

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,41 @@
 
 pub use self::error::{Error, Result};
 
-use std::net::SocketAddr;
-use axum::{Router, middleware};
-use axum::extract::{Query, Path};
+use crate::ctx::Ctx;
+use crate::log::log_request;
+use crate::model::ModelController;
+use axum::extract::{Path, Query};
+use axum::http::{Method, Uri};
 use axum::response::{Html, IntoResponse, Response};
 use axum::routing::{get, get_service};
+use axum::{middleware, Json, Router};
 use serde::Deserialize;
+use serde_json::json;
+use std::net::SocketAddr;
 use tower_cookies::CookieManagerLayer;
 use tower_http::services::ServeDir;
+use uuid::Uuid;
 
 mod error;
 mod web;
+mod model;
+mod ctx;
+mod log;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
+    let mc = ModelController::new().await?;
+
+    let routes_apis = web::routes_tickets::routes(mc.clone())
+		.route_layer(middleware::from_fn(web::mw_auth::mw_require_auth));
+
     let routes_all = Router::new()
         .merge(routes_hello())
         .merge(web::routes_login::routes())
+        .nest("/api", web::routes_tickets::routes(mc.clone()))
         .layer(middleware::map_response(main_resp_mapper))
         .layer(CookieManagerLayer::new())
-        .fallback_service(route_static());    
+        .fallback_service(route_static());  
 
     // region:      --- Start Server ---
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
@@ -34,13 +49,48 @@ async fn main() {
         .await
         .unwrap();
     // endregion:   --- Start Server ---
+    Ok(())
 }
 
-async fn main_resp_mapper(res: Response) -> Response {
-    println!("->> {:<12} - main_resp_mapper - {res:?}", "RES_MAPPER");
+async fn main_resp_mapper(
+    ctx: Option<Ctx>,
+	uri: Uri,
+	req_method: Method,
+	res: Response,
+) -> Response {
+    println!("->> {:<12} - main_response_mapper", "RES_MAPPER");
+	let uuid = Uuid::new_v4();
 
-    println!();
-    res
+	// -- Get the eventual response error.
+	let service_error = res.extensions().get::<Error>();
+	let client_status_error = service_error.map(|se| se.client_status_and_error());
+
+	// -- If client error, build the new reponse.
+	let error_response =
+		client_status_error
+			.as_ref()
+			.map(|(status_code, client_error)| {
+				let client_error_body = json!({
+					"error": {
+						"type": client_error.as_ref(),
+						"req_uuid": uuid.to_string(),
+					}
+				});
+
+				println!("    ->> client_error_body: {client_error_body}");
+
+				// Build the new response from the client_error_body
+				(*status_code, Json(client_error_body)).into_response()
+			});
+
+	// Build and log the server log line.
+	let client_error = client_status_error.unzip().1;
+	// TODO: Need to hander if log_request fail (but should not fail request)
+	let _ =
+		log_request(uuid, req_method, uri, ctx, service_error, client_error).await;
+
+	println!();
+	error_response.unwrap_or(res)
 
 }
 
